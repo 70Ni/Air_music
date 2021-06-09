87b7055f402e46becc48b0f5bb3291acfe9d4efa
70Ni <this4development@gmail.com>
2021-06-09 11:16:14 +0530

    search field functions added to header

--- src/Components/Dashbar/DashBar.js
+++ src/Components/Dashbar/DashBar.js
@@ -1,31 +1,51 @@
-import React from 'react';
+import React,{Component} from 'react';
 import User from '../../Images/Icons/User.svg';
 
 import search from '../../Images/Icons/search.svg';
 import bell from '../../Images/Icons/bell.svg';
+import { Music } from '../../Json/Music';
+
 
 import './DashBar.css'
 
-function DashBar() {
-    return (
-        <div className="DashBar_wrapper">
-            <div className="DashBar_content">
-                <div className="Browser_wrapper">
-                    <input type="text" id="searchField" name="Search" placeholder="Browse" />
-                    <img className="Browser_icon" src={search} />
-                </div>
-                <div className="DashIcon_wrapper">
-                    <div className="user_icon_wrapper">
-                        <img src={User} />
+class DashBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            search:''
+        }
+    }
+
+    searchChange = (event) => {
+        this.setState({
+            search:event.target.value
+        })
+        const searchedMusics = Music.filter(item=>{
+            return item.name.toLowerCase().includes(this.state.search.toLowerCase())
+        })
+        console.log(searchedMusics)
+    }
+    render() {
+        return (
+            <div className="DashBar_wrapper">
+                <div className="DashBar_content">
+                    <div className="Browser_wrapper">
+                        <input type="text" id="searchField" name="Search" placeholder="Browse" onChange={this.searchChange} />
+                        <img className="Browser_icon" src={search} />
                     </div>
-                    <div className="bell_icon_wrapper">
-                        <sapn className="notifier"></sapn>
-                        <img src={bell} />
+                    <div className="DashIcon_wrapper">
+                        <div className="user_icon_wrapper">
+                            <img src={User} />
+                        </div>
+                        <div className="bell_icon_wrapper">
+                            <sapn className="notifier"></sapn>
+                            <img src={bell} />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        );
+    }
 }
 
-export default DashBar
+export default DashBar;
