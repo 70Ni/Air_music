e625a182454f9b5054d3a44f2885acab9d289ce6
70Ni <this4development@gmail.com>
2021-06-11 15:48:56 +0530

    set the DurationTime

--- src/Components/MusicPlayer/DurationTime.js
+++ src/Components/MusicPlayer/DurationTime.js
@@ -1,30 +1,33 @@
 
 
 import React, { Component } from 'react'
-import './MusicPlayer.css'
 import { seektimeupdate } from '../../Container/FUNCTIONS'
 import { connect } from 'react-redux';
-import setCurrentMusics from '../../Redux/Actions/musicPlayer.action'
+import './MusicPlayer.css'
 
 
 let intervalID = undefined;
+
 console.log(intervalID)
+
 class DurationTime extends Component {
     constructor(props) {
         super(props);
         this.state = {}
     }
+
     componentDidMount() {
 
         intervalID = setInterval(() => {
             seektimeupdate()
 
-       }, 1000);
+        }, 1000);
+    }
 
-        }
     componentWillUnmount() {
         clearInterval(intervalID)
     }
+
     render() {
         return (
             <div className="MusicDuration_wrapper">{this.props.currentTimer.curmins}:{this.props.currentTimer.cursecs}/{this.props.currentTimer.durmins}:{this.props.currentTimer.dursecs} </div>
