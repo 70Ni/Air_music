18dd3ad73189fc319ba4dad9c9eed094bbb77779
70Ni <this4development@gmail.com>
2021-06-15 20:49:41 +0530

    Recent page function

--- src/Container/RecentPage/RecentList.js
+++ src/Container/RecentPage/RecentList.js
@@ -7,34 +7,26 @@ import { connect } from 'react-redux';
 
 
 
-let HistoryArray = [5, 8, 15, 56]
-let searchedMusics = undefined;
-let c = Music.map(item=> (
-     HistoryArray.map(song => song == item.id))
+let HistoryArray = []
 
-  );
 
-  let a = Music.map(song=> song.id == HistoryArray.map(x => x))
-let h = HistoryArray.map(x=>x)
-let m = Music.map(song=> song.id)
-
+let History = Music.filter(song => HistoryArray.includes(song.id))
 
 class RecentList extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
     }
-    componentDidMount(){
-        // HistoryArray.push(this.props.currentState.MusicHistory)
+    componentDidMount() {
+        HistoryArray.push(this.props.currentState.MusicHistory)
     }
     render() {
-        console.log(h)
-        console.log(m)
+        console.log(History)
+
         return (
 
-            <div className="TrenPag_LisCarWrapper" style={{color:'white'}}>
-                {/* {
-                    this.props.currentState.MusicHistory.map((song, i) => {
+            <div className="TrenPag_LisCarWrapper" style={{ color: 'white' }}>
+                {
+                    History.map((song, i) => {
                         return (
                             <ListcardB
                                 key={i}
@@ -50,8 +42,7 @@ class RecentList extends Component {
                             />
                         )
                     })
-                } */}
-                Hello
+                }
             </div>
         )
 
--- src/Redux/Reducer/musicPlayer.reducer.js
+++ src/Redux/Reducer/musicPlayer.reducer.js
@@ -8,7 +8,8 @@ const musicGroupReducer = (state = InitialState, action) => {
     switch (action.type) {
         case 'SET_MUSIC_PLAYER':
             state.prevMusic = action.payload.prevMusic
-            let History = state.MusicHistory.push(action.payload.prevMusic)
+            let History = state.MusicHistory.push(action.payload.prevMusic);
+            state.MusicHistory.push(History)
             return Object.assign({}, state, { CurrentGroup:action.payload })
         case 'SET_NEXT_MUSIC':
             return Object.assign({}, state, { IndexOfMusic:state.IndexOfMusic+1 })
