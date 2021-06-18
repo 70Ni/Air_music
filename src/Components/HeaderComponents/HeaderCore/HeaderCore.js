4cfd75d1cc12184808acd4b27e6ee21fead8670a
70Ni <this4development@gmail.com>
2021-06-18 14:40:50 +0530

    added search Redux

--- src/Components/HeaderComponents/HeaderCore/HeaderCore.js
+++ src/Components/HeaderComponents/HeaderCore/HeaderCore.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import thumb from '../../../Music files/Carlie Puth/Thumbnails/La Girl.jpg'
 
@@ -8,34 +8,40 @@ const mapStateToProps = state => ({
     resultMusic: state.search.SearchResults
 })
 
-let currentMusic = [];
-const HeaderCore = ({ currentState }) => {
-    useEffect(() => {
-            console.log("Hello")
-    },[currentState.CurrentGroup.IndexOfMusic]);
-    console.log(currentMusic)
-    return (
-        <div className="HeaderCore_wrapper">
-            <div className="HeaderCore_content_Inactive">
-                <div className="HeaderCore_Intro">
-                    <div className="HeaderCore_header">
-                        <div className="Nav_header_name">air</div>
-                        <div className="Nav_header_BB">Musics</div>
-                    </div>
-                    {currentState.prevMusic ? null : <button className="getStarted">Get Started</button>}
-                    <img src={thumb} alt="" className="Thumb_image" />
-                    <div className="HeaderCore_para">
-                        Air Music is a music streaming service developed for knowledge and entertainment purposes.
+class HeaderCore extends Component {
+    constructor(props) {
+        super(props);   
+        this.state = {}
+    }
+
+    componentDidMount(){
+        
+    }
+    render() {
+            const {currentState} = this.props
+            const item = currentState.CurrentGroup.MusicGroup
+            return (
+                <div className="HeaderCore_wrapper">
+                    <div className="HeaderCore_content_Inactive">
+                        <div className="HeaderCore_Intro">
+                            <div className="HeaderCore_header">
+                                <div className="Nav_header_name">air</div>
+                                <div className="Nav_header_BB">Musics</div>
+                            </div>
+                            {item ? null: <button className="getStarted">Get Started</button>}
+                            <img src={item ? item[currentState.CurrentGroup.IndexOfMusic].preview:null} alt="" className="Thumb_image" />
+                            <div className="HeaderCore_para">
+                                Air Music is a music streaming service developed for knowledge and entertainment purposes.
+                            </div>
+
+                            <div className="Music_wrpaer">
+                                <div className="HeaderCore_MusicName">Power</div>
+                            </div>
+                            <div className="NowListening_para">Now Listening...</div>
+                        </div>
                     </div>
 
-                    <div className="Music_wrpaer">
-                        <div className="HeaderCore_MusicName">Power</div>
-                    </div>
-                    <div className="NowListening_para">Now Listening...</div>
-                </div>
-            </div>
-
-            {/* <div className="HeaderCore_content_Inactive">
+                    {/* <div className="HeaderCore_content_Inactive">
                 <div className="HeaderCore_Intro">
                     <div className="HeaderCore_header ">
                         <div className="Nav_header_name">air</div>
@@ -48,8 +54,10 @@ const HeaderCore = ({ currentState }) => {
                     <div className="NowListening_para">Now Listening...</div>
                 </div>
             </div> */}
-        </div>
-    )
-}
+                </div>
+            );
+        }
+    }
 
-export default connect(mapStateToProps, null)(HeaderCore);
+
+    export default connect(mapStateToProps, null)(HeaderCore);
