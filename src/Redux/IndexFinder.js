4c55349b3edbee38f478003c7bcc2e61f3b7a5bd
70Ni <this4development@gmail.com>
2021-07-07 12:58:25 +0530

    Redux added skip id

--- src/Redux/IndexFinder.js
+++ src/Redux/IndexFinder.js
@@ -12,23 +12,27 @@ const IndexSlice = createSlice({
             if (state.ClickedMusic !== payload.id) {
                 let r = payload.Group.group.map(toPlay => toPlay.id === payload.id).indexOf(true);
                 state.ClickedMusic = payload.id
-                state.MusicGroup = payload.Group.group
+                state.MusicGroup = payload.Group
                 state.IndexOfMusic = r
             }
         },
         SkipPrev(state) {
-            if (state.IndexOfMusic > 1) {
-                state.IndexOfMusic -= 1
+            if (state.IndexOfMusic > 0) {
+                let next = state.IndexOfMusic - 1
+                state.IndexOfMusic = next
+                state.ClickedMusic = state.MusicGroup.group[next].id
             }
         },
         SkipNext(state) {
-            if (state.IndexOfMusic + 1 < state.MusicGroup.length) {
-                state.IndexOfMusic += 1
+            if (state.IndexOfMusic + 1 < state.MusicGroup.group.length) {
+                let prev =state.IndexOfMusic + 1
+                state.IndexOfMusic = prev
+                state.ClickedMusic = state.MusicGroup.group[prev].id
             }
         },
         AutoPlay(state) {
-            let change = !state.MusicGroup[0].isLoop
-            state.MusicGroup[0].isLoop = change
+            let change = !state.MusicGroup.isLoop
+            state.MusicGroup.isLoop = change
         }
 
     }
