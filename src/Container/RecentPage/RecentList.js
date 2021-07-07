2ff3daed3ba58de5d0bd2b6076d0f56b888875e7
70Ni <this4development@gmail.com>
2021-07-07 19:48:12 +0530

    history added

--- src/Container/RecentPage/RecentList.js
+++ src/Container/RecentPage/RecentList.js
@@ -1,49 +1,41 @@
-import React, { useEffect, Component } from 'react';
+import React, { useEffect } from 'react';
 import { shuffle, numFormatter } from '../../Container/FUNCTIONS'
 import ListcardB from '../../Components/Cards/ListCard/ListCardB'
 
 import { Music } from '../../Json/Music';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 
 
 
-let HistoryArray = []
-class RecentList extends Component {
-    constructor(props) {
-        super(props);
-    }
 
-    render() {
-        let History = Music.filter(song => this.props.currentState.MusicHistory.includes(song.id))
-        return (
+function RecentList() {
 
-            <div className="TrenPag_LisCarWrapper" style={{ color: 'white' }}>
-                {
-                    History.map((song, i) => {
-                        return (
-                            <ListcardB
-                                key={i}
-                                id={song.id}
-                                preview={song.Preview.default}
-                                name={song.name}
-                                artist={song.Artist}
-                                artist_image={song.Artist_image}
-                                views={numFormatter(song.Views)}
-                                likes={numFormatter(song.Likes)}
-                                duration={song.duration}
-                                URL={song.URL}
-                            />
-                        )
-                    })
-                }
-            </div>
-        )
+    const MusicLoaded = useSelector(state => state.MusicLoaded.History);
+    let History = Music.filter(song => MusicLoaded.includes(song.id))
+    return (
 
-    }
+        <div className="TrenPag_LisCarWrapper" style={{ color: 'white' }}>
+            {
+                History.map((song, i) => {
+                    return (
+                        <ListcardB
+                            key={i}
+                            id={song.id}
+                            preview={song.Preview.default}
+                            name={song.name}
+                            artist={song.Artist}
+                            artist_image={song.Artist_image}
+                            views={numFormatter(song.Views)}
+                            likes={numFormatter(song.Likes)}
+                            duration={song.duration}
+                            URL={song.URL}
+                        />
+                    )
+                })
+            }
+        </div>
+    )
+
 }
-const mapStateToProps = state => ({
-    currentState: state.player,
-})
-
-export default connect(mapStateToProps, null)(RecentList);
+export default RecentList;
