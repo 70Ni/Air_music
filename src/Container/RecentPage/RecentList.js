9c17d224d59552a3668f13309e90182cc297c4dc
70Ni <this4development@gmail.com>
2021-07-14 15:31:32 +0530

    History changes

--- src/Container/RecentPage/RecentList.js
+++ src/Container/RecentPage/RecentList.js
@@ -13,6 +13,8 @@ function RecentList() {
 
     const MusicLoaded = useSelector(state => state.MusicLoaded.History);
     let History = Music.filter(song => MusicLoaded.includes(song.id))
+
+    let array = {group: History, isLoop:false}
     return (
 
         <div className="TrenPag_LisCarWrapper" style={{ color: 'white' }}>
@@ -20,6 +22,7 @@ function RecentList() {
                 History.map((song, i) => {
                     return (
                         <ListcardB
+                            newarray = {array}
                             key={i}
                             id={song.id}
                             preview={song.Preview.default}
--- src/Pages/RecentPage.js
+++ src/Pages/RecentPage.js
@@ -5,38 +5,27 @@ import RecentList from '../Container/RecentPage/RecentList';
 
 import './PlayListPage.css'
 import { connect } from 'react-redux';
-class RecentPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
 
-    }
+function RecentPage(params) {
 
-    render() {
-        return (
-            <div className="Trending_page_wrapper" style={{ 'maxWidth': '1440px' }}>
-                <div className="searchData_appear_wrapper">
-                    <div className="SUBheader">Recent</div>
+    return (
+        <div className="Trending_page_wrapper" style={{ 'maxWidth': '1440px' }}>
+            <div className="searchData_appear_wrapper">
+                <div className="SUBheader">Recent</div>
 
-                </div>
-                <div className="TrenPag_LisCarWrapper">
-                    <RecentList />
-                </div>
-                <div className="disPag_listButton">
-                    <div className="AutoPlayButton">
-                        <div className="Play_D">back to top</div>
-                        <img className="ButtonImg" src={AutoPlay} alt="" />
-                    </div>
+            </div>
+            <div className="TrenPag_LisCarWrapper">
+                <RecentList />
+            </div>
+
+            <div className="disPag_listButton">
+                <div className="AutoPlayButton">
+                    <div className="Play_D">back to top</div>
+                    <img className="ButtonImg" src={AutoPlay} alt="" />
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
-const mapDispatchToProps = dispatch => ({
-    setCurrentMusic: musicItem => dispatch(setCurrentMusics(musicItem))
-})
-const mapStateToProps = state => ({
-    currentState: state.player,
-})
 
-export default connect(mapStateToProps, mapDispatchToProps)(RecentPage);
+export default RecentPage;
