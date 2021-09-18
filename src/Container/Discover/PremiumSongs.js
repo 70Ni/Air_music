98b496f8292111cd613da69234a2c705b9df2558
70Ni <4thedevelopments@gmail.com>
2021-09-18 22:10:18 +0530

    changin discover page media screen nav

--- src/Container/Discover/PremiumSongs.js
+++ src/Container/Discover/PremiumSongs.js
@@ -1,51 +1,40 @@
-import React, { useEffect, useMemo, useState } from 'react';
-import { shuffle, numFormatter } from '../../Container/FUNCTIONS'
-import SqureCard from '../../Components/Cards/SqureCard/SqureCard'
+import React, { useEffect, useMemo, useState } from "react";
+import { shuffle, numFormatter } from "../../Container/FUNCTIONS";
+import SqureCard from "../../Components/Cards/SqureCard/SqureCard";
 
-import { Music } from '../../Json/Music';
-
-
-
-let ShuffledArray =() => shuffle(Music);
+import { Music } from "../../Json/Music";
 
 const PremiumSongs = () => {
+  const [matches, setMatches] = useState(6);
 
-    const [matches, setMatches] = useState(6)
+  let ShuffledArray = () => shuffle(Music);
 
+  useEffect(() => {
+    if (window.matchMedia("(max-width: 1086px)").matches) {
+      setMatches(18);
+    }
+  }, [window.innerWidth]);
+  const array2 = { group: ShuffledArray().slice(0, matches), isLoop: false };
+  return (
+    <div className="SqureCa_wrapper">
+      {array2.group.map((song, i) => {
+        return (
+          <SqureCard
+            key={i}
+            newarray={array2}
+            id={song.id}
+            preview={song.Preview.default}
+            name={song.name}
+            artist={song.Artist}
+            views={numFormatter(song.Views)}
+            likes={numFormatter(song.Likes)}
+            duration={song.duration}
+            URL={song.URL}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
-
-    useEffect(() => {
-
-        if (window.matchMedia("(max-width: 1086px)").matches) {
-            setMatches(18)
-        }
-    }, [window.innerWidth])
-    const array2 = { group: ShuffledArray().slice(0, 6), isLoop:false };
-    return (
-
-        <div className="SqureCa_wrapper">
-            {
-                array2.group.map((song, i) => {
-                    return (
-                        <SqureCard
-                            key={i}
-                            newarray = {array2}
-                            id={song.id}
-                            preview={song.Preview.default}
-                            name={song.name}
-                            artist={song.Artist}
-                            views={numFormatter(song.Views)}
-                            likes={numFormatter(song.Likes)}
-                            duration={song.duration}
-                            URL={song.URL}
-                        />
-                    )
-                })
-            }
-        </div>
-    );
-}
-
-
-
-export default PremiumSongs ;
+export default PremiumSongs;
--- src/Container/Discover/RelatedSongs.js
+++ src/Container/Discover/RelatedSongs.js
@@ -1,41 +1,32 @@
-import React, { useMemo } from 'react';
-import ListCardB from '../../Components/Cards/ListCard/ListCardB';
-import { shuffle, numFormatter } from '../../Container/FUNCTIONS'
+import React, { useMemo } from "react";
+import ListCardB from "../../Components/Cards/ListCard/ListCardB";
+import { shuffle, numFormatter } from "../../Container/FUNCTIONS";
 
-import { Music } from '../../Json/Music';
-
-
-let ShuffledArray = shuffle(Music);
-
+import { Music } from "../../Json/Music";
 
 const RelatedSongs = () => {
-
-    const array = { group: ShuffledArray.slice(0, 6), isLoop: false }
-    console.log("Related")
-    return (
-        <div>
-            {
-
-                array.group.map((song, i) => {
-                    return (
-                        <ListCardB
-                            newarray={array}
-                            key={i}
-                            id={song.id}
-                            preview={song.Preview.default}
-                            name={song.name}
-                            artist={song.Artist}
-                            views={numFormatter(song.Views)}
-                            likes={numFormatter(song.Likes)}
-                            duration={song.duration}
-                            URL={song.URL}
-                        />
-                    )
-                })
-            }
-        </div>
-    );
-}
-
+  let ShuffledArray = () => shuffle(Music);
+  const array = { group: ShuffledArray().slice(0, 6), isLoop: false };
+  return (
+    <div>
+      {array.group.map((song, i) => {
+        return (
+          <ListCardB
+            newarray={array}
+            key={i}
+            id={song.id}
+            preview={song.Preview.default}
+            name={song.name}
+            artist={song.Artist}
+            views={numFormatter(song.Views)}
+            likes={numFormatter(song.Likes)}
+            duration={song.duration}
+            URL={song.URL}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default RelatedSongs;
