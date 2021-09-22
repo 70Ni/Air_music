4c2f25f3705834fb2e13bb873246cae55151f90a
70Ni <4thedevelopments@gmail.com>
2021-09-22 20:05:45 +0530

    Changing landing page

--- src/Components/Auth/siginIn/SignIn.jsx
+++ src/Components/Auth/siginIn/SignIn.jsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Inputs from "../Inputs/Inputs";
 import Icon from "../../../Images/Icons/googleIcon.svg";
 import GoogleIcon from "../../../Images/Icons/GoogleIcon";
+import "./Signin.css";
 
 function SignIn() {
+  const [isSignUp, setisSignUp] = useState(false);
   return (
     <div className="signIn_Wrapper">
-      <div className="signHeader">Sign In</div>
-      <Inputs type="email" placeholder="user@email.com" label="Email" />
-      <Inputs type="password" placeholder="password" label="Password" />
-      <Inputs type="submit" value="Sign in" className="signInbutton" />
-      <div className="orsingIntxt">or sign in with </div>
-      <div className="goobuttonwrpr">
-        <button className="googleButton">
-          <GoogleIcon />
-          <div className="googlesigntxt">Sign in with Google</div>
-        </button>
+      <div className="AuthContnt">
+        <div className="signHeader">Sign In</div>
+        <Inputs type="email" placeholder="user@email.com" label="Email" />
+        <Inputs type="password" placeholder="password" label="Password" />
+        <Inputs type="submit" value="Sign in" className="signInbutton" />
+        <div className="orsingIntxt">or sign in with </div>
+        <div className="goobuttonwrpr">
+          <button className="googleButton">
+            <GoogleIcon />
+            <div className="googlesigntxt">Sign in with Google</div>
+          </button>
+        </div>
       </div>
     </div>
   );
