220e1597f35a6f92a88f2adde1cc5d85487df6e7
Tony Emmanuel <this4development@gmail.com>
2021-11-16 20:06:16 +0530

    adding

--- src/Components/Cards/TrendingCard/TrendingListCard.jsx
+++ src/Components/Cards/TrendingCard/TrendingListCard.jsx
@@ -4,7 +4,6 @@ function TrendingListCard() {
     return (
         <div className="TrendingListCa_wrapper">
             <div className="Hello"></div>
-            <div className="someBrna"></div>
         </div>
     )
 }
