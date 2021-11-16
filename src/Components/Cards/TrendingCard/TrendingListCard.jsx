bb96ed81794c37cb4127481d2e305e11f8b969fc
Tony Emmanuel <this4development@gmail.com>
2021-11-16 19:58:09 +0530

    adding

---
+++ src/Components/Cards/TrendingCard/TrendingListCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TrendingListCard() {
+    return (
+        <div className="TrendingListCa_wrapper">
+            <div className="Hello"></div>
+            
+        </div>
+    )
+}
+
+export default TrendingListCard
