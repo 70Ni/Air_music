aab4f75bc0f4b612fecb774262b2757ede5f0f6b
Tony Emmanuel <this4development@gmail.com>
2021-11-18 23:05:39 +0530

    treding one changes

--- src/Components/Cards/TrendingCard/TrendingOne.jsx
+++ src/Components/Cards/TrendingCard/TrendingOne.jsx
@@ -10,24 +10,6 @@ function TrendingOne() {
     <div className="TrendingOneWrapper">
       <div className="DiscoverCard_filter"></div>
       <img src={Image} className="TrendingOne_image" />
-      {/* <div className="trOne_cont_wrapper">
-        <div className="TrendingOne_content">
-          <div className="trOne_lower">
-            <div className="TrendingOne_TextWrapper">
-              <div className="TrendingOne_number">Trending #1</div>
-              <div className="trending_name">New Man</div>
-            </div>
-            <div className="TrendingOne_play">
-              <div className="AutoPlayButton">
-                <div className="Play_D">Play Now</div>
-              </div>
-            </div>
-          </div>
-          <div className="trOne_upper">
-            <img src={Favorite} alt="save" className="List_save" />
-          </div>
-        </div>
-      </div> */}
       <div className="trenigOne_contentwrpr">
         <div className="trendingOne_content">
           <div className="TreninglstCa_fa">
