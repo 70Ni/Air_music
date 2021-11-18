ae08c95a02f437e85f7cc9b7314d06d1d0d130b5
Tony Emmanuel <this4development@gmail.com>
2021-11-18 22:42:05 +0530

    adding trendingOne Card changes

--- src/Components/Cards/TrendingCard/TrendingOne.jsx
+++ src/Components/Cards/TrendingCard/TrendingOne.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import "./TrendingOne.css";
 import Image from "../../../Images/artists/billieeilish-e1580227219759-800x450.jpg";
 import Favorite from "../../../Images/Icons/save.svg";
+import Play from "../../../Images/Icons/play_white.svg";
 
 function TrendingOne() {
   return (
@@ -27,6 +28,21 @@ function TrendingOne() {
           </div>
         </div>
       </div> */}
+      <div className="trenigOne_contentwrpr">
+        <div className="trendingOne_content">
+          <div className="TreninglstCa_fa">
+            <img src={Favorite} alt="favorite" className="List_save" />
+          </div>
+          <div className="trdgOne_det_wrpr">
+            <div className="trenOne_number">Trending #1</div>
+            <div className="TrgOne_name">New Man</div>
+          </div>
+        </div>
+        <button className="trgOne_button">
+          <div className="Play_D">Play</div>
+          <img className="ButtonImg" src={Play} alt="" />
+        </button>
+      </div>
     </div>
   );
 }
