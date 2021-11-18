ec71d866108444d02684bd8cfb6d1218eeff417c
Tony Emmanuel <this4development@gmail.com>
2021-11-19 00:16:44 +0530

    treding page changes

--- src/Pages/TrendingPage.js
+++ src/Pages/TrendingPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import TrendingSongs from "../Container/Trending/TrendingSongs";
 import TrendingCard from "../Components/Cards/TrendingCard/TrendingCard";
@@ -11,49 +11,63 @@ import SqureB from "../Components/Cards/SqureCard/SqureB";
 import ListCardB from "../Components/Cards/ListCard/ListCardB";
 import RelatedSongs from "../Container/Discover/RelatedSongs";
 function TrendingPage() {
+  const [listView, setlistView] = useState(false);
   return (
     <div className="Trending_page_wrapper" style={{ maxWidth: "1440px" }}>
       <div className="TrenPag_LisCarWrapper">
         <div className="trgListCardHedr_wrpr">
-          <div className="SUBheader">Item View</div>
-          <div className="SUBheader">List View</div>
+          <div
+            className="SUBheader"
+            onClick={() => setlistView(false)}
+            style={listView ? { color: "white" } : { color: "#F27E4C" }}
+          >
+            Item View
+          </div>
+          <div
+            className="SUBheader"
+            onClick={() => setlistView(true)}
+            style={listView ? { color: "#F27E4C" } : { color: "white" }}
+          >
+            List View
+          </div>
         </div>
-        <div className="trending_sectionA">
-          <div className="TrendingInto">
-            <TrendingOne />
-            <TrendingArtist />
-          </div>
-          <div className="TrendingLstCard_wrappr">
-            <div className="TredglstCard_AB_wrpr">
-              <div className="TredglstCard_A">
-                <TrendingListCard />
-                <TrendingListCard />
-              </div>
-              <div className="TredglstCard_B">
-                <TrendingListCard />
-                <TrendingListCard />
-              </div>
+        {listView ? (
+          <div className="trending_sectionB">
+            <div className="TrendingInto">
+              <RelatedSongs />
+              <TrendingArtist />
             </div>
-
-            {/* <TrendingSongs /> */}
           </div>
-          <div className="SUBheader">Other Musics</div>
-          {/* <div className="trend_otherMusic_wrpr">
+        ) : (
+          <div className="trending_sectionA">
+            <div className="TrendingInto">
+              <TrendingOne />
+              <TrendingArtist />
+            </div>
+            <div className="TrendingLstCard_wrappr">
+              <div className="TredglstCard_AB_wrpr">
+                <div className="TredglstCard_A">
+                  <TrendingListCard />
+                  <TrendingListCard />
+                </div>
+                <div className="TredglstCard_B">
+                  <TrendingListCard />
+                  <TrendingListCard />
+                </div>
+              </div>
+
+              {/* <TrendingSongs /> */}
+            </div>
+            <div className="SUBheader">Other Musics</div>
+            {/* <div className="trend_otherMusic_wrpr">
             <SqureB />
             <SqureB />
             <SqureB />
             <SqureB />
             <SqureB />
           </div> */}
-        </div>
-
-        {/* ///////////////// */}
-        <div className="trending_sectionB">
-          <div className="TrendingInto">
-            <RelatedSongs />
-            <TrendingArtist />
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
