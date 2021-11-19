d7a6a313afc1418092d96cdb1c83ab35b4519c88
Tony Emmanuel <this4development@gmail.com>
2021-11-20 00:39:21 +0530

    Lyrics component changes

--- src/Components/Lyrics/Lyrics.jsx
+++ src/Components/Lyrics/Lyrics.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React,{useState} from "react";
 import "./Lyrics.css";
 
 import image from "../../Images/DiscovPage/trending.jpg";
 import expand from "../../Images/Icons/expand.svg";
 function Lyrics() {
+  const [expander, setexpander] = useState(500);
   return (
     <div className="Lyrics_Wrapper">
       <div className="lyrics_content">
@@ -12,13 +13,13 @@ function Lyrics() {
         </div>
         <div className="lyricsHdr_wrp">
           <div className="SUBheader">Lyrics</div>
-          <img src={expand} alt="" />
+          <img src={expand} alt="expand" onClick={()=>setexpander(200)} />
         </div>
         <div className="lyrics_lenght_wrpr">
           <div className="lyrics_length"></div>
         </div>
         <div className="lyrics_focus_wrpr"></div>
-        <div className="lyrics_box_wrpr">
+        <div className="lyrics_box_wrpr" style={{height:expander}}>
           <div className="lyrics_txt">
             Rebekah rode up on the afternoon train It was sunny Her saltbox
             house on the coast Took her mind off St. Louis Bill was the heir to
